6aa67f794cfa075c2101315ebc8e5fd058d05a51
Andyxa <spirinandrei90@gmail.com>
2025-10-10 00:43:07 +0500

    user register with bugs

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI, APIRouter
 from app.api.v1.hello_world import hello_world_router
-
+from app.api.v1.User.user import user_router
 
 app = FastAPI()
-app.include_router(hello_world_router)
+base_router = APIRouter(prefix="/api/v1", tags=["base"])
+
+
+base_router.include_router(hello_world_router)
+base_router.include_router(user_router, prefix="/users")
+
+
+app.include_router(base_router)
